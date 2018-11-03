75ded9c96ea6a9b7bd4e4f148473c41a5c3c59f6
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2018-11-03 04:38:32 +0200

    the whole project working

---
+++ Drive Organization Tool/Main Menu.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Drive_Organization_Tool
+{
+    partial class Main_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderview = new System.Windows.Forms.TreeView();
+            this.pop_up = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.print = new System.Windows.Forms.Button();
+            this.pop_up.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // folderview
+            // 
+            this.folderview.HideSelection = false;
+            this.folderview.Location = new System.Drawing.Point(11, 71);
+            this.folderview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.folderview.Name = "folderview";
+            this.folderview.Size = new System.Drawing.Size(331, 360);
+            this.folderview.TabIndex = 0;
+            this.folderview.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.folderview_BeforeExpand);
+            this.folderview.DragDrop += new System.Windows.Forms.DragEventHandler(this.folderview_DragDrop);
+            this.folderview.DragOver += new System.Windows.Forms.DragEventHandler(this.folderview_DragOver);
+            this.folderview.MouseDown += new System.Windows.Forms.MouseEventHandler(this.folderview_MouseDown);
+            // 
+            // pop_up
+            // 
+            this.pop_up.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.pop_up.Name = "pop_up";
+            this.pop_up.Size = new System.Drawing.Size(153, 136);
+            this.pop_up.Text = "Pop";
+            this.pop_up.Opening += new System.ComponentModel.CancelEventHandler(this.pop_up_Opening);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Enabled = false;
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(377, 232);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(195, 44);
+            this.print.TabIndex = 2;
+            this.print.Text = "Make XML File & CLose";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // Main_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 439);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.folderview);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Main_Menu";
+            this.Text = "Main Menu";
+            this.Load += new System.EventHandler(this.Main_Menu_Load);
+            this.pop_up.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView folderview;
+        private System.Windows.Forms.ContextMenuStrip pop_up;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Button print;
+    }
+}
+
---
+++ Drive Organization Tool/Main Menu.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace Drive_Organization_Tool
+{
+    public partial class Main_Menu : Form
+    {
+        public int count = 0;
+        private string path_to_cut_from;
+        private string path_to_copy_from;
+        public Main_Menu()
+        {
+            InitializeComponent();
+            folderview.AllowDrop = true;
+            path_to_copy_from = "";
+            path_to_cut_from = "";
+        }
+        private void Main_Menu_Load(object sender, EventArgs e)
+        {
+            folderview.Nodes.Clear();
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                DriveInfo temp = new DriveInfo(drive);
+                TreeNode node = new TreeNode(drive.Substring(0, 1));
+                node.Tag = drive;
+                node.Name = temp.Name;
+                if (temp.IsReady == true)
+                {
+                    node.Nodes.Add("...");
+                    folderview.Nodes.Add(node);
+                }
+            }
+
+        }
+        private void folderview_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                if (e.Node.Nodes.Count > 0)
+                {
+                    e.Node.Nodes.Clear();
+                    string[] arr = Directory.GetDirectories(e.Node.Tag.ToString());
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        DirectoryInfo info = new DirectoryInfo(arr[i]);
+                        TreeNode node = new TreeNode(info.Name);
+                        if ((info.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+                        node.Tag = arr[i];
+                        node.Name = info.Name;
+                        e.Node.Nodes.Add(node);
+                        node.Nodes.Add("...");
+                    }
+                }
+                string[] arr2 = Directory.GetFiles(e.Node.Tag.ToString());
+                foreach (string x in arr2)
+                {
+                    DirectoryInfo info = new DirectoryInfo(x);
+                    TreeNode node = new TreeNode(info.Name);
+                    if ((info.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+                    node.Name = info.Name;
+                    node.Tag = x;
+                    e.Node.Nodes.Add(node);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void folderview_MouseDown(object sender, MouseEventArgs e)
+        {
+            TreeNode node = folderview.GetNodeAt(e.X, e.Y);
+            folderview.SelectedNode = node;
+            if (e.Button == MouseButtons.Right)
+            {
+                if (folderview.SelectedNode == null) return;
+                bool isDrive = false;
+                string[] drives = Environment.GetLogicalDrives();
+                foreach (string drive in drives)
+                    if (drive == folderview.SelectedNode.Tag.ToString()) isDrive = true;
+                if (isDrive) return;
+                if (path_to_copy_from.Length > 0 || path_to_cut_from.Length > 0)
+                    pop_up.Items[3].Enabled = true;
+                else
+                    pop_up.Items[3].Enabled = false;
+                if (File.Exists(folderview.SelectedNode.Tag.ToString()) == true) pop_up.Items[3].Enabled = false;
+                if (isDrive) pop_up.Items[2].Enabled = false;
+                pop_up.Show(MousePosition);
+                return;
+            }
+            else
+            {
+                if (node == null) return;
+                string[] drives = Environment.GetLogicalDrives();
+                foreach (string drive in drives)
+                {
+                    if (node.Tag.ToString() == drive) return;
+                }
+                if (node != null) folderview.DoDragDrop(node, DragDropEffects.Copy);
+            }
+        }
+        private void folderview_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            TreeNode nodeSource = (TreeNode)e.Data.GetData(typeof(TreeNode));
+            if (nodeSource != null)
+            {
+                Point pt = new Point(e.X, e.Y);
+                pt = folderview.PointToClient(pt);
+                TreeNode nodeTarget = folderview.GetNodeAt(pt);
+                if (nodeTarget != null)
+                {
+                    if (Directory.Exists(nodeTarget.Tag.ToString()) == false) return;
+                    e.Effect = DragDropEffects.Copy;
+                    folderview.SelectedNode = nodeTarget;
+                }
+            }
+        }
+        private void folderview_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                TreeView tree = (TreeView)sender;
+                Point pt = new Point(e.X, e.Y);
+                pt = tree.PointToClient(pt);
+                TreeNode nodeTarget = tree.GetNodeAt(pt);
+                TreeNode nodeSource = (TreeNode)e.Data.GetData(typeof(TreeNode));
+                nodeTarget.Expand();
+                string f = @"\";
+                f += nodeSource.Text;
+                bool done = Move_Dragged(nodeSource.Tag.ToString(), nodeTarget.Tag.ToString(), f);
+                if (done)
+                    nodeTarget.Nodes.Add((TreeNode)nodeSource.Clone());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private bool Move_Dragged(string path_source, string path_target, string name)
+        {
+            bool isFile = File.Exists(path_source);
+            try
+            {
+                if (isFile)
+                    File.Move(path_source, path_target + name);
+                else
+                    Directory.Move(path_source, path_target + name);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                return false;
+            }
+        }
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] drives = Environment.GetLogicalDrives();
+                foreach (string drive in drives)
+                    if (drive == folderview.SelectedNode.Tag.ToString()) return;
+                path_to_copy_from = "";
+                path_to_cut_from = folderview.SelectedNode.Tag.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] drives = Environment.GetLogicalDrives();
+                foreach (string drive in drives)
+                    if (drive == folderview.SelectedNode.Tag.ToString()) return;
+                path_to_cut_from = "";
+                path_to_copy_from = folderview.SelectedNode.Tag.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name = @"\";
+            string target = folderview.SelectedNode.Tag.ToString();
+            if (path_to_copy_from.Length > 0)
+            {
+                try
+                {
+                    bool isFile = File.Exists(path_to_copy_from);
+                    if (!isFile)
+                    {
+                        DirectoryInfo info = new DirectoryInfo(path_to_copy_from);
+                        name += info.Name;
+                        DirectoryCopy(path_to_copy_from, target + name, true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else if (path_to_cut_from.Length > 0)
+            {
+                try
+                {
+                    bool isFile = File.Exists(path_to_cut_from);
+                    if (!isFile)
+                    {
+                        DirectoryInfo info = new DirectoryInfo(path_to_cut_from);
+                        name += info.Name;
+                        Directory.Move(path_to_cut_from, target + name);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string drive in drives)
+                if (drive == folderview.SelectedNode.Tag.ToString()) return;
+            try
+            {
+                bool isFile = File.Exists(folderview.SelectedNode.Tag.ToString());
+                if (isFile)
+                {
+                    File.Delete(folderview.SelectedNode.Tag.ToString());
+                }
+                else
+                {
+                    Directory.Delete(folderview.SelectedNode.Tag.ToString(), true);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        private void print_Click(object sender, EventArgs e)
+        {
+            if (folderview.SelectedNode == null)
+            {
+                MessageBox.Show("Select something");
+                return;
+            }
+            string path = folderview.SelectedNode.Tag.ToString();
+            Make_XML(path);
+        }
+        private void Make_XML(string path)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.CheckCharacters = false;
+            settings.NewLineHandling = NewLineHandling.None;
+            DirectoryInfo info = new DirectoryInfo(path);
+            XmlWriter textWriter = XmlWriter.Create("OrganizerLastReport.Xml", settings);
+            textWriter.WriteStartDocument();
+            textWriter.WriteStartElement("Root_Node");
+            if (info.FullName.Length == 3)
+            {
+                textWriter.WriteStartAttribute("Drive");
+                textWriter.WriteAttributeString("Name", info.Name.Substring(0, 1));
+                Explore(info, textWriter);
+            }
+            else if (File.Exists(info.FullName))
+            {
+                textWriter.WriteStartAttribute("File");
+                textWriter.WriteAttributeString("Name", info.Name.ToString());
+            }
+            else
+            {
+                textWriter.WriteStartAttribute("Folder");
+                textWriter.WriteAttributeString("Name", info.Name.ToString());
+                Explore(info,textWriter);
+            }
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+            this.Close();
+        }
+        private void Explore(DirectoryInfo node , XmlWriter writer)
+        {
+            try
+            {
+                foreach (DirectoryInfo i in node.GetDirectories())
+                {
+                    if( (i.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+                    writer.WriteStartElement("Directory");
+                    writer.WriteAttributeString("Name", i.Name.ToString());
+                    foreach (FileInfo j in i.GetFiles())
+                    {
+                        writer.WriteStartElement("File");
+                        writer.WriteAttributeString("Name", j.Name.ToString());
+                        writer.WriteEndElement();
+                    }
+                    Explore(i, writer);
+                    writer.WriteEndElement();
+                }
+            }
+            catch(UnauthorizedAccessException)
+            {
+
+            }
+        }
+
+        private void pop_up_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+
+}
